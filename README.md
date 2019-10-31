1297d0e6a2e86c94755c8ef633ba538c259e4672
shipeng <370119797@qq.com>
2019-10-31 14:40:30 +0800

    修改脚本

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-Minify PNG, JPEG images with imagemin
+# 使用gulp-imagemin压缩cocos creator项目里的图片
 
-Install:
-$ npm install
+### 安装:  
+npm install
 
-Usage:
-1. setup path.json
-2. $ npm run imagemin
+### 使用方法:
+1. 配置path.json
+2. 运行gulp 
+
+---------------
+### Nginx的配置心得：
+#### 一、压缩设置
+1. 编辑nginx.conf
+```
+    gzip  on;
+    gzip_min_length 1000;
+    gzip_buffers 4 16k;
+    gzip_comp_level 2;
+    gzip_types text/plain application/x-javascript text/css application/xml text/javascript application/json application/javascript font/ttf application/octet-stream;
+    gzip_vary on;
+    gzip_disable "MSIE [1-6].";
+```
+2. 重新加载Nginx  
+    ps -ef|grep nginx
+    /usr/sbin/nginx -s reload    
+    
+3. 用curl测试Gzip是否成功开启  
+    curl -I -H "Accept-Encoding: gzip, deflate" "http://xxx"
+    
+    
+#### 二、缓存设置
+1. 在server块前配置  
+```
+    map $sent_http_content_type $expires {
+        default                    off;
+        text/html                  epoch;
+        text/css                   max;
+        application/javascript     max;
+        application/json           max;
+        ~image/                    max;
+    }
+```
+2. 在server块里增加配置  
+expires $expires;
+
+3. 重新加载Nginx  
+/usr/sbin/nginx -s reload
+
+4. 用curl测试
+    curl -I "http://xxx"
--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,20 @@ const imagemin = require('gulp-imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 const cache = require('gulp-cache');
 const path = require('path');
+const zip = require('gulp-zip');
+const moment = require("moment")
+const argv = require('minimist')(process.argv.slice(2));
+const exec = require('gulp-exec');
+const spawn = require('child_process').spawn;
+const ffmpeg = require('gulp-fluent-ffmpeg');
+
+let config = require(path.resolve("path.json"));
+let rootPath = path.resolve(config.path);
+let resPath = path.join(rootPath, './res');
 
 function compressImage() {
-    let config = require(path.resolve("path.json"));
-    
-    let srcPath = path.resolve(config.src);
-    srcPath = path.join(srcPath, './**/*.{png,jpg,gif,ico}');
-    console.log('src:', srcPath);
-    
-    let destPath = path.resolve(config.dest);
-    console.log('dest:', destPath);
+    let srcPath = path.join(resPath, './**/*.{png,jpg,gif,ico}');
+    console.log('path:', srcPath);
     
     return gulp.src(srcPath)
 		.pipe(cache(imagemin([
@@ -20,12 +24,36 @@ function compressImage() {
             // imagemin.optipng({optimizationLevel: 5})
             imageminPngquant()
         ])))
-		.pipe(gulp.dest(destPath));
+		.pipe(gulp.dest(resPath));
 }
 
-exports.default = compressImage
+function archiveFile() {
+    let timeStamp = moment().format("YYYY-MM-D_HH-mm-ss");
+    let zipPath = path.join(rootPath, "../")
+    return gulp.src(path.join(rootPath, "./**/*"), {base:'build'})
+        .pipe(zip(`client_${timeStamp}.zip`))
+        .pipe(gulp.dest(zipPath));
+}
+
+function compressAudio() {
+    let srcPath = path.join(resPath, './**/*.mp3');
+    return gulp.src(srcPath)
+		.pipe(ffmpeg('mp3', function (cmd) {
+          return cmd
+            // .audioBitrate('128k')
+            .audioChannels(2)
+            .audioCodec('libmp3lame')
+            .audioQuality(8)
+        }))
+		.pipe(gulp.dest(resPath));
+}
 
 exports.clear = () => {
     cache.clearAll();
     return Promise.resolve('done');
 }
+
+exports.compress = compressImage;
+exports.compressAudio = compressAudio;
+exports.archive = archiveFile;
+exports.default = series(compressImage, archiveFile)
